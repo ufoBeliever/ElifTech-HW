8bfd294b3b26b32c93682a60ba3e57aeca3b0f93
Dmitriy Iudin <dmtry.iudin@gmail.com>
2022-06-04 12:25:04 +0300

    hw#2 done

---
+++ HW#2/task1.js
@@ -0,0 +1,21 @@
+/*
+* Задача 1:
+* Виводьте "Hello World" безкінечно починаючи з затримки 1 секунда, але з кожним разом додаємо затримку в 1 секунду. Так другий раз затримка буде 2 секунди, третій раз 3 секунди і т.д.
+* Додайте значення затримки в виведеному повідомлені.
+* Hello World. 1
+* Hello World. 2
+* Hello World. 3
+* ....
+*
+* Обмеження: можна використовувати тільки const (без let і var)
+*/
+
+module.exports = function func(delay, logDelay){
+    if(delay>=1000000000) return
+    setTimeout(()=>{
+        console.log(`Hello World. ${logDelay/1000}`)
+    }, delay)
+    return func(delay+logDelay+1000, logDelay+1000)
+}
+
+
---
+++ HW#2/task2.js
@@ -0,0 +1,25 @@
+/*
+* Задача 2:
+* Так само, як і в першій задачі, але цього разу з повторами затримок.
+* Виведіть Hello World 5 разів із затримкою 100 ms.
+* Потім виведіть цю стрінгу знову 5 разів із затримкою 200ms.
+* Потім знову 5 разів із затримкою 300ms.
+* І так далі, поки програма не буде зупинена. Так само в вивід додайте значення затримки
+*/
+
+const obj = {
+    counter: 0
+}
+
+module.exports = function func(delay, logDelay){
+    if(delay>=100000000)return
+    setTimeout(()=>{
+        console.log('Hello World', logDelay)
+    }, delay)
+    obj.counter++
+    if(obj.counter>=5){
+        obj.counter=0
+        return func(delay+logDelay+100, logDelay+100)
+    }
+    func(delay, logDelay)
+}
