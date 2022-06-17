5fa05f978b239555edc168b3fbb4da1510de4a42
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-06-17 13:59:29 +0300

    .

--- HW#4/main/index.js
+++ HW#4/main/index.js
@@ -1,10 +1,29 @@
 const http = require('http')
 
-const server = http.createServer()
 
-server.on('request', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain'})
-    res.end(`Server is running, method: ${req.method}`)
-})
-
-server.listen(3000,'localhost' , ()=>console.log('server is running...'))
+http.createServer((req, res)=>{
+    if(req.method==="GET"){
+        res.writeHead(200, {'Content-Type':'text/plain'})
+        res.end('Method: GET');
+    }
+    else if(req.method==="POST"){
+        res.writeHead(200, {'Content-Type':'text/plain'})
+        res.end('Method: POST');
+    }
+    else if(req.method==="PUT"){
+        res.writeHead(200, {'Content-Type':'text/plain'})
+        res.end('Method: PUT');
+    }
+    else if(req.method==="PATCH"){
+        res.writeHead(200, {'Content-Type':'text/plain'})
+        res.end('Method: PATCH');
+    }
+    else if(req.method==="DEL"){
+        res.writeHead(200, {'Content-Type':'text/plain'})
+        res.end('Method: DEL');
+    }
+    else {
+        res.writeHead(200, {'Content-Type':'text/plain'})
+        res.end(`Method: ${req.method}`);
+    }
+}).listen(5000)
